8b44e013dfa463d17784076c6d3ea0cc2d1b7aff
spatialdatasciencegroup <spatialdatasciencegroup@gmail.com>
2018-11-15 19:16:56 -0600

    first commit

---
+++ app/src/main/java/com/example/arpan/ezpay/login_page.java
@@ -0,0 +1,13 @@
+package com.example.arpan.ezpay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_page extends AppCompatActivity {
+//3366ff blue
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
